7f18a555ba8d6cacf3f744c9f2bc77a6541ec035
Raphael Krebs <raphael.krebs@outlook.com>
2024-05-27 09:42:36 +0200

    basic implementation of dashboard frontend

---
+++ wwwroot/js/app/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("stellarIO", ["stellarService"]);
+
+
+
+app.controller("dashboardController", ["$scope", "planetService", function ($scope, planetService) {
+    planetService.getMyPlanets().then(function (myPlanets) {
+        $scope.myPlanets = myPlanets;
+    }, function (error) {
+
+    });
+}]);
---
+++ wwwroot/js/app/service.js
@@ -0,0 +1,100 @@
+﻿var appService = angular.module("stellarService", []);
+
+appService.service("stellarApi", ["$http", "$q", function ($http, $q) {
+    var getUrl = function (path, version) {
+        var url = "/api/";
+        if (version) {
+            url += version + "/";
+        }
+        else {
+            url += "v1/";
+        }
+        url += path;
+        return url;
+    }
+    return {
+        url: function (path, version) {
+            return getUrl(path, version);
+        },
+        get: function (path, version) {
+            var deferred = $q.defer();
+
+            var url = getUrl(path, version);
+            $http.get(url).then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (error) {
+                    deferred.reject(error);
+                }
+            )
+
+            return deferred.promise;
+        },
+        post: function (path, data, version) {
+            var deferred = $q.defer();
+
+            var url = getUrl(path, version);
+
+
+            $http.post(url, data).then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (error) {
+                    deferred.reject(error);
+                }
+            )
+
+
+            return deferred.promise;
+        },
+        put: function (path, data, version) {
+            var deferred = $q.defer();
+
+            var url = getUrl(path, version);
+
+
+            $http.put(url, data).then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (error) {
+                    deferred.reject(error);
+                }
+            )
+
+
+            return deferred.promise;
+        },
+        delete: function (path, version) {
+            var deferred = $q.defer();
+
+            var url = getUrl(path, version);
+            $http.delete(url).then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (error) {
+                    deferred.reject(error);
+                }
+            )
+
+            return deferred.promise;
+        }
+    }
+}]);
+
+appService.service("planetService", ["$http", "$q", "stellarApi", function ($http, $q, stellarApi) {
+    return {
+        getPlanets: function () {
+            return stellarApi.get("planet");
+        },
+        getMyPlanets: function () {
+            return stellarApi.get("planet/mine");
+        },
+        getPlanet: function (planetId) {
+            return stellarApi.get("planet/" + planetId);
+        }
+    }
+}])
